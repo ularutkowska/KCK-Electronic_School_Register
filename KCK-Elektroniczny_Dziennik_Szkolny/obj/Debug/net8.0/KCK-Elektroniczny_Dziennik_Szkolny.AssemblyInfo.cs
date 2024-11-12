4b31dcf699a1629dc522da1ff53ad820795f86f3
wi113506 <86154@student.pb.edu.pl>
2024-11-12 15:03:08 +0100

    Dokumentacja

--- KCK-Elektroniczny_Dziennik_Szkolny/obj/Debug/net8.0/KCK-Elektroniczny_Dziennik_Szkolny.AssemblyInfo.cs
+++ KCK-Elektroniczny_Dziennik_Szkolny/obj/Debug/net8.0/KCK-Elektroniczny_Dziennik_Szkolny.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KCK-Elektroniczny_Dziennik_Szkolny")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6a19c4db22b50f6d5c4a718d5c732a0ae525dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb17152ac9f9458d76b2a5088e01a6ddcb15993")]
 [assembly: System.Reflection.AssemblyProductAttribute("KCK-Elektroniczny_Dziennik_Szkolny")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KCK-Elektroniczny_Dziennik_Szkolny")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
